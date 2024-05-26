817bbae5f0f9df9d884c147ac4913c66ca797c8f
EUNSOL0313 <liat0313@gmail.com>
2024-05-26 22:22:56 +0900

    email 수정

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -8,7 +8,7 @@ const useController = {}
 useController.createUser = async (req, res) => {
    try {
       const { email, name, password } = req.body
-      const user = await User.findOne({ email })
+      const user = await User.findOne({ email: email })
       if (user) {
          throw new Error('이미 가입이 된 유저 입니다.')
       }
@@ -43,7 +43,7 @@ useController.loginWithEmail = async (req, res) => {
       }
       throw new Error('아이디 또는 비밀번호가 일치하지 않습니다.')
    } catch (error) {
-      res.status(400).json({ status: 'fail', error })
+      res.status(400).json({ status: 'fail', message: error.message })
    }
 }
 
